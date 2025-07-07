d3cd42093cfc60d5e774d0949a0a53edc8a9d3b5
Ankush-verma-Source <ankushverma100009@gmail.com>
2025-07-07 12:54:02 +0530

    express added

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port=3000;
+
+
+app.use((req,res)=>{
+    res.send("server is running");
+})
+
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})

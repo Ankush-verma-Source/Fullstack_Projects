dce384ee588928decbd1d5589155b9aa58c16962
Ankush-verma-Source <ankushverma100009@gmail.com>
2025-07-07 13:14:13 +0530

    style.css added

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const port=process.env.PORT;
+const path = require('path');
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
 
 
 app.use((req,res)=>{
-    res.send("server is running");
+    res.render("index");
 })
+
+
+
+
+
 
 
 app.listen(port,()=>{
